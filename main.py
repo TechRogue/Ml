c399ef6e5c8085d626aaceeea17afa6867d5e582
TechRogue <hazikkhalid700@gmail.com>
2025-06-01 20:09:10 +0530

    fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy as np 
 
-from sklearn.model_selection import train_testsplit , GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import MinMaxScalar 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
