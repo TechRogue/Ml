6cc10b4686b5ffd2d2243a1787a4b7b4440f860d
TechRogue <hazikkhalid700@gmail.com>
2025-06-01 20:07:43 +0530

    import

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ import numpy as np
 from sklearn.model_selection import train_testsplit , GridSearchCV
 from sklearn.preprocessing import MinMaxScalar 
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import matplotlib.pyplot as pit 
+import seaborn as sns 
 
