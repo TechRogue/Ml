53fbd98051cef77bb99d929d8271d04120d97d4c
TechRogue <hazikkhalid700@gmail.com>
2025-06-01 20:02:45 +0530

    ai

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numbpy as np 
+
+from sklearn.model_selection import train_testsplit , GridSearchCV
+from sklearn.preprocessing import MinMaxScalar 
+from sklearn.neighbours import MinMaxScalar 
